91340454cc5a9cfe7ddc62f09d682ca5cf35fde1
sosyuki <294535375@qq.com>
2015-12-15 16:50:08 +0800

    test 共享收货地址

--- wechat_jssdk/jssdk_test.tpl.php
+++ wechat_jssdk/jssdk_test.tpl.php
@@ -1,16 +1,36 @@
 <button class="btn btn-default jssdk-test">JSSDK TEST</button>
+<?php
 
+$redirect_uri = 'http://dining2.demo.sosyuki.com/foodshopcunt/5';
+
+
+$tools = new JsApiPay();
+$tools->Set_redirect_uri($redirect_uri);
+$editAddress = $tools->GetEditAddressParameters();
+?>
 <script type="text/javascript">
+
   (function ($) {
+
+    function editAddress() {
+      WeixinJSBridge.invoke(
+        'editAddress',
+        <?php echo $editAddress; ?>,
+        function (res) {
+          var value1 = res.proviceFirstStageName;
+          var value2 = res.addressCitySecondStageName;
+          var value3 = res.addressCountiesThirdStageName;
+          var value4 = res.addressDetailInfo;
+          var tel = res.telNumber;
+
+          alert(value1 + value2 + value3 + value4 + ":" + tel);
+        }
+      );
+    }
+
     $(document).ready(function () {
       $('button.jssdk-test').on('click',function(){
-        wx.scanQRCode({
-          needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
-          scanType: ["qrCode", "barCode"], // 可以指定扫二维码还是一维码，默认二者都有
-          success: function (res) {
-            var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
-          }
-        });
+        editAddress();
       });
     });
   })(jQuery);
