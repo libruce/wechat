bae06a62c86bf8d66ddbca5899281acd49013923
sosyuki <294535375@qq.com>
2015-12-15 17:25:05 +0800

    test 共享收货地址

--- wechat_jssdk/jssdk_test.tpl.php
+++ wechat_jssdk/jssdk_test.tpl.php
@@ -10,29 +10,29 @@ $tools->Set_redirect_uri($redirect_uri);
 $editAddress = $tools->GetEditAddressParameters();
 ?>
 <script type="text/javascript">
-
-  (function ($) {
-
-    function editAddress() {
-      WeixinJSBridge.invoke(
-        'editAddress',
-        <?php echo $editAddress; ?>,
-        function (res) {
-          var value1 = res.proviceFirstStageName;
-          var value2 = res.addressCitySecondStageName;
-          var value3 = res.addressCountiesThirdStageName;
-          var value4 = res.addressDetailInfo;
-          var tel = res.nationalCode;
-
-          alert(value1 + value2 + value3 + value4 + ":" + tel);
-        }
-      );
-    }
-
-    $(document).ready(function () {
-      $('button.jssdk-test').on('click', function () {
-        editAddress();
-      });
-    });
-  })(jQuery);
+//
+//  (function ($) {
+//
+//    function editAddress() {
+//      WeixinJSBridge.invoke(
+//        'editAddress',
+//        <?php //echo $editAddress; ?>//,
+//        function (res) {
+//          var value1 = res.proviceFirstStageName;
+//          var value2 = res.addressCitySecondStageName;
+//          var value3 = res.addressCountiesThirdStageName;
+//          var value4 = res.addressDetailInfo;
+//          var tel = res.nationalCode;
+//
+//          alert(value1 + value2 + value3 + value4 + ":" + tel);
+//        }
+//      );
+//    }
+//
+//    $(document).ready(function () {
+//      $('button.jssdk-test').on('click', function () {
+//        editAddress();
+//      });
+//    });
+//  })(jQuery);
 </script>
