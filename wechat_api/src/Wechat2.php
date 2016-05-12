825e9c91dd795d156caad0d8a9dff1068ea58318
sosyuki <294535375@qq.com>
2016-05-12 11:35:11 +0800

    $aid

---
+++ wechat_api/src/Wechat2.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sosyuki
+ * Date: 2016/5/12
+ * Time: 9:47
+ */
+
+namespace Drupal\wechat_api;
+
+/**
+ * Class YukiWechat
+ * @package Drupal\wechat_api
+ * 微信基础类库
+ */
+class Wechat2 {
+
+}
