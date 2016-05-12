b5de330d9938adad0b5c93f48a2a53fa254ad1b8
sosyuki <294535375@qq.com>
2016-05-12 22:49:35 +0800

    test

--- wechat_api/src/Wechat2.php
+++ wechat_api/src/Wechat2.php
@@ -27,8 +27,8 @@ class Wechat2 extends Wechat {
     watchdog('api_data', $data);
     //$data = self::json_encode($data);
     //$data = json_encode($data, JSON_UNESCAPED_UNICODE);
-    $result = $this->http_post(self::API_URL_PREFIX . self::CUSTOM_SEND_URL . 'access_token=' . $this->access_token, self::json_encode($data));
-
+//    $result = $this->http_post(self::API_URL_PREFIX . self::CUSTOM_SEND_URL . 'access_token=' . $this->access_token, self::json_encode($data));
+    $result = 'aa';
     watchdog('$result', json_encode(($result)));
     if ($result) {
       $json = json_decode($result, TRUE);
