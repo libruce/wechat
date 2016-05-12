2762dc7bd08a76761ee72fab8209d3fdae8ea7f2
sosyuki <294535375@qq.com>
2016-05-12 22:41:13 +0800

    test

--- wechat_api/src/Wechat.php
+++ wechat_api/src/Wechat.php
@@ -2447,9 +2447,9 @@ class Wechat {
    * @return boolean|array
    */
   public function sendCustomMessage($data) {
-    if (!$this->access_token && !$this->checkAuth()) {
-      return FALSE;
-    }
+//    if (!$this->access_token && !$this->checkAuth()) {
+//      return FALSE;
+//    }
     //$data = self::json_encode($data);
     //$data = json_encode($data, JSON_UNESCAPED_UNICODE);
     $result = $this->http_post(self::API_URL_PREFIX . self::CUSTOM_SEND_URL . 'access_token=' . 'cWqSBMhAb4_DYSKRWpNN80dz3hGPKtl30WTAuxWjj-vgPHwff5rHJieGOnQy0HO3H351-z3UbwSUgGg4NGS7eO-KG9MgOE5KAY0_ErA-trWvSFL7SbHE0wDH5OX8BlyjWYQiAMDACU', self::json_encode($data));
