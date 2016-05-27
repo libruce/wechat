a9dc2ac5638808487ddb7e7259713c732b7cde0f
sosyuki <294535375@qq.com>
2016-05-27 17:42:12 +0800

    test

--- wechat_scan/src/ScanController.php
+++ wechat_scan/src/ScanController.php
@@ -10,5 +10,42 @@ namespace Drupal\wechat_scan;
 
 
 class ScanController {
-//https://api.weixin.qq.com/scan/product/create?access_token=tT8VqJWVX4C-WvIG41tyelMclMZsAPFbLxbECsJvIghceqQCMB83hI5bxcInUR-amUrVsKq4bcBmF9PgooNrFA%20
+
+  function createProduct($data) {
+    $data = drupal_json_encode($data);
+    $url = "https://api.weixin.qq.com/scan/product/create?access_token={$this->access_token()}";
+    return $this->request($url, 'POST', $data);
+  }
+
+  protected function request($url, $method = 'GET', $data = '', $timeout = 60) {
+    try {
+      $request = drupal_http_request($url, array(
+        'headers' => array(
+          'Content-Type' => 'application/json',
+        ),
+        'method' => $method,
+        'data' => $data,
+        'timeout' => $timeout,
+        'context' => [],
+      ));
+      if ($request->code != 200) {
+        throw new \Exception('网络错误');
+      }
+
+      $data = $request->data;
+      $data = drupal_json_decode($data);
+
+      if ($data['errcode'] != 0) {
+        throw new \Exception($data['errmsg'], $data['errcode']);
+      }
+      return $data;
+    } catch (\Exception $e) {
+      watchdog_exception('微信扫一扫接口异常跟踪', $e);
+      throw new $e;
+    }
+  }
+
+  protected function access_token() {
+    return wechat_api_get_access_token();
+  }
 }
