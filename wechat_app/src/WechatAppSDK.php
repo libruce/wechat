d2da39005c27e4e047ea2557ca3fdaa9a3a8183e
sosyuki <294535375@qq.com>
2016-09-23 13:45:17 +0800

    和sdk关联

---
+++ wechat_app/src/WechatAppSDK.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sosyuki
+ * Date: 2016/9/23
+ * Time: 13:40
+ */
+
+namespace Drupal\wechat_app;
+
+
+use Drupal\wechat_api\Wechat;
+
+class WechatAppSDK extends  Wechat{
+
+}
